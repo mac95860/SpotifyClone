e92efd734b69e9925f8f6805b0222b784d90f3d9
Michael Cervantes <mac95860@hotmail.com>
2021-01-15 18:09:06 -0600

    adding some hooks

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Pages/Login'
+import Player from './Pages/Player/Player';
 import {getTokenFromUrl} from './util/spofify'
 import SpotifyWebApi from 'spotify-web-api-js';
+import { useDataLayerValue } from './util/StateProvider';
+
 
 const spotify = new SpotifyWebApi();
 
 function App() {
 
   const [token, setToken] = useState(null);
+  const [ {user}, dispatch ] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -17,21 +21,25 @@ function App() {
 
     if(_token){
       setToken(_token);
-
       spotify.setAccessToken(_token);
-      spotify.getMe().then(user => {
-        console.log('User: ', user);
-      })
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
     }
 
     console.log('I HAVE A TOKEN>>>> ', token)
   }, []);
 
+  console.log("THE USER IS>>>", user);
+
   return (
     <div className="App">
       {
         token ? (
-          <h1>I am logged in</h1>
+          <Player/>
         ) : (
           <Login/>
         )
---
+++ spotify-clone/src/util/reducer.js
@@ -0,0 +1,25 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+}
+
+// state is the what is currently in the datalayer, action is setting the latest state
+const reducer = (state, action) => {
+    console.log(action);
+
+    switch(action.type) {
+        case 'SET_USER':
+          //actions > type, [payload]  
+            return {
+                //keep what is in current state
+                ...state,
+                user: action.user,
+            }
+        default: 
+            return state;
+    }
+}
+
+export default reducer;
